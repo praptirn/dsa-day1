e79e0eab9ee86ec27d1cf62ca877b34baa5d768e
Prapti Nayak <praptinayak2005@gmail.com>
2026-02-21 06:12:46 +0000

    q2 done

--- assignments/q2.cpp
+++ assignments/q2.cpp
@@ -21,17 +21,14 @@ vector<int> maxSlidingWindow(vector<int>& nums, int k) {
 
     for(int i = 0; i < nums.size(); i++) {
 
-        // Remove indices خارج the window
         while(!dq.empty() && dq.front() <= i - k)
             dq.pop_front();
 
-        // Remove smaller elements from back
         while(!dq.empty() && nums[dq.back()] <= nums[i])
             dq.pop_back();
 
         dq.push_back(i);
 
-        // Window formed
         if(i >= k - 1)
             ans.push_back(nums[dq.front()]);
     }
